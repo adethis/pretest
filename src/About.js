6e6d95ffabb23f0c433c150b00557acbf8752fb6
adethis <adet91@gmail.com>
2019-06-18 10:31:44 +0700

    File pretest

---
+++ src/About.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class About extends Component {
+	render() {
+		return (
+			<div>
+				<h2>About</h2>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta asperiores iure molestiae eaque vero quisquam totam, 
+				minus delectus maxime, modi voluptatum velit eos atque fugiat provident, recusandae unde aperiam. Accusamus.</p>
+			</div>
+		);
+	}
+}
+
+export default About;
